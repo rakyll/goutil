5a22a6880105eaf83e7fe6ab88f6b07b50c40e11
Jaana Burcu Dogan <jbd@google.com>
2017-07-01 21:31:53 -0700

    initial commit

---
+++ pprofutil/http.go
@@ -0,0 +1,28 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package pprofutil contains helpers for runtime/pprof.
+package pprofutil
+
+import (
+	"context"
+	"net/http"
+	"runtime/pprof"
+)
+
+// LabelHandler adds profiler labels to the given handler.
+func LabelHandler(h http.Handler) http.Handler {
+	return &labelHandler{orig: h}
+}
+
+type labelHandler struct {
+	orig http.Handler
+}
+
+func (l *labelHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	labels := pprof.Labels("handler", r.URL.Path)
+	pprof.Do(r.Context(), labels, func(ctx context.Context) {
+		l.orig.ServeHTTP(w, r)
+	})
+}
